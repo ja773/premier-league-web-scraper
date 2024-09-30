08b36fda2dca38b9d109fe0f38936cc14ac0f0cf
Jiyaad Ali <jiyaadali@outlook.com>
2024-09-30 03:49:17 +0100

    add url fetching from webpage

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from bs4 import BeautifulSoup
 standings_url = 'https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats'
 
 data = requests.get(standings_url)
-print(data.text)
+
+soup = BeautifulSoup(data.text)
+standings_table = soup.select('table.stats_table')[0]
+links = standings_table.find_all('a')
+links = [l.get('href') for l in links]
+links = [l for l in links if '/squads/' in l]
+team_urls = [f'https://fbref.com{l}' for l in links]
+
+
