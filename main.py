39bfd8dc50d5ac56d3e13f21e7d88d8949a0dd56
ja773 <ja773@cam.ac.uk>
2024-09-30 22:36:04 +0100

    Tested html parsing for one team url

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ standings_url = 'https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League
 
 data = requests.get(standings_url)
 
-soup = BeautifulSoup(data.text)
+soup = BeautifulSoup(data.text, features = 'html.parser')
 standings_table = soup.select('table.stats_table')[0]
 links = standings_table.find_all('a')
 links = [l.get('href') for l in links]
 links = [l for l in links if '/squads/' in l]
 team_urls = [f'https://fbref.com{l}' for l in links]
+
+team_url = team_urls[0]
+data = requests.get(team_url)
+
+matches = pd.read_html(data.text, match = 'Scores & Fixtures')
+print(matches)
 
 
